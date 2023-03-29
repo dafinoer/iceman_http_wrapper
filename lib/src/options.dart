4d49d780e021a6d017ea08b05d5c58dde42b0faa
Muhammad Dafi <dafinoer@outlook.com>
2023-03-29 13:02:30 +0700

    revoke singleton

--- lib/src/options.dart
+++ lib/src/options.dart
@@ -2,21 +2,10 @@ class Options {
   final String baseUrl;
   final int timeOut;
 
-  static Options? _instance;
-
-  Options._({
+  Options({
     required this.baseUrl,
     required this.timeOut,
   });
-
-  factory Options({required String baseUrl, int? timeOut}) {
-    _instance ??= Options._(
-      baseUrl: baseUrl,
-      timeOut: timeOut ?? 5000,
-    );
-
-    return _instance!;
-  }
 
   String get host => Uri.parse(baseUrl).host;
 }
